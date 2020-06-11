122c0bcf7d63661d98647e564f81f82c9b8b6750
true65536 <66752659+true65536@users.noreply.github.com>
2020-06-11 09:58:49 -0700

    48828125

--- js/game_manager.js
+++ js/game_manager.js
@@ -59,7 +59,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 5 : 10;
+    var value = Math.random() < 0.9 ? 50 : 55;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
